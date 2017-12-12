1c2d52eb63a9c3b8a9c2560cf42520e5572caf4a
Unknown <koynanthiya369@gmail.com>
2017-12-12 12:46:58 +0700

    แก้ข้อที่5ครึ่งนึง

--- lab_5_3.cpp
+++ lab_5_3.cpp
@@ -10,8 +10,14 @@ int main() {
     cout << endl;
     if(score > 80) {
         cout << "Grade 4" << endl;
-    } else if (score > 70) {
+        } else if (score > 70) {
         cout << "Grade 3" << endl;
+        } else if (score > 60) {
+        cout << "Grade 2" << endl;
+        } else if (score >= 50) {
+        cout << "Grade 1" << endl;
+        } else if (score <= 20) {
+        cout << "Grade 0" << endl;
     }
     return 0;
 }
--- lab_5_5.cpp
+++ lab_5_5.cpp
@@ -3,8 +3,11 @@
 
 int main () {
 
-  string playerName;
+  string playerName ;
   int score = 0, life = 3;
+  char choice1
+  char choice2
+  char choice3
 
   // ประกาศตัวแปรเพื่อรับการเลือกคำตอบของผู้เล่น
   // เช่นการเป็น ตัวเลขให้ประกาศเป็น int choice1;
@@ -13,13 +16,27 @@ int main () {
 
   cout << "Welcome to Mini Adventure Game." << endl;
   cout << "Please enter your name : ";
+  cin >>  playerName;
 
   // เขียนคำสั่งรับชื่อ แล้วเก็บในตัวแปร playerName
-
+  if (playerName.length() > 8) {
+        cout << "Your name is too long!!!!" << endl;
+        playerName.erase (playerName.begin()+8, playerName.end());
+        cout << "Your name has been changed to " << playerName << endl;
+    }
+        else {cout << playerName << endl}
+        }
 
   // เขียนคำสั่งแสดงผล คำบรรยายสภาพแวดล้อม สถานการณ์ และคำทักทายที่มีชื่อตัวละคร
-  // เช่น cout << "Hello " << playerName << ". You are in Robinson. You are walking happily. Suddenly you realize that forgetting the wallet."
-  // << " But you're lazy to get back to get it." << endl;
+  cout << "Hello " << playerName << ". You are in Big C. You are walking happily. Suddenly you realize that forgetting the wallet."
+  << " But you're lazy to get back to get it." << endl;
+
+  cout << "Come out of BIG C" << playerName << ". You went to Japan happily, suddenly I saw someone stolen."
+  << " Are you thinking of helping him catch a good thief." << endl;
+
+
+  cout << "From Japan" << playerName << "You go to Khao Yai to see the atmosphere you are hungry now."
+  << " Where do you think you are going to eat." << endl;
 
 
   // เขียนคำสั่งแสดงผล คำถามให้ผู้ใช้ตัดสินใจ
@@ -28,12 +45,38 @@ int main () {
 
   // เขียนคำสั่งรับคำตอบจากผู้ใช้ แล้วให้คะแนนหรือทำโทษ จากการตัดสินใจนั้น
   // เช่น
-  //     cin >> choice1;
-  //     switch(choice1) {
-  //        case "S" : score += 10; life -= 1; cout << "You stole money from people. Now you can shopping.";
-  //        case "A" : score += 50; cout << "You ask for money from people then you get 100 baht. You are very happy. Now you can shopping."
-  //     }
+      cin >> choice1;
+         case "A"
+            score += 10;
+            cout << "You ask for money from people then you get 100 baht. You are very happy. Now you can shopping."
+            switch(choice1) {
+        case "S" :
+            score += 50;
+            life -= 1;
+            cout << "You stole money from people. Now you can shopping.";
 
+
+        cin >> choice2;
+         case "A" :
+            score += 10;
+            cout << "You help him catch the thief."
+            switch(choice1) {
+        case "S" :
+            score += 50;
+            life -= 1;
+            cout << "You do not help him catch the thief.";
+
+
+
+        cin >> choice3;
+         case "A" :
+            score += 10;
+            cout << "Ask for food from tourists"
+            switch(choice1) {
+        case "S" :
+            score += 50;
+            life -= 1;
+            cout << "Steal food from tourists.";
 
 
   // ฝึกสร้างสถานการณ์และสร้างเงื่อนไข อีก 2 สถานการณ์
